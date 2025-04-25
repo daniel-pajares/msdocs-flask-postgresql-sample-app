76b2dc9f193f0b8eab1332d732b153be8d120e38
Daniel Pajares Carrasco <124221748+daniel-pajares@users.noreply.github.com>
2025-04-25 06:27:13 +0000

    Configure Azure database connecton

--- azureproject/production.py
+++ azureproject/production.py
@@ -1,16 +1,13 @@
 import os
 
-# Validar que todas las variables de entorno necesarias estén configuradas
-required_env_vars = ['AZURE_POSTGRESQL_USER', 'AZURE_POSTGRESQL_PASSWORD', 'AZURE_POSTGRESQL_HOST', 'AZURE_POSTGRESQL_NAME']
-missing_vars = [var for var in required_env_vars if not os.getenv(var)]
-
-if missing_vars:
-    raise EnvironmentError(f"Faltan las siguientes variables de entorno requeridas: {', '.join(missing_vars)}")
-
-# Construir la URI de la base de datos
+# Configuración de la URI de la base de datos para producción
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
     dbuser=os.getenv('AZURE_POSTGRESQL_USER'),
     dbpass=os.getenv('AZURE_POSTGRESQL_PASSWORD'),
     dbhost=os.getenv('AZURE_POSTGRESQL_HOST'),
     dbname=os.getenv('AZURE_POSTGRESQL_NAME')
 )
+
+# Otras configuraciones específicas del entorno de producción
+DEBUG = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
